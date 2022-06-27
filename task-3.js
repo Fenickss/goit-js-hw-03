3a5bfb5c49ab4270e126677f8ac46f7bccddaca1
Fenickss <denis.suxov.1991@mail.ru>
2022-06-27 14:53:06 +0300

    task

--- task-3.js
+++ task-3.js
@@ -40,28 +40,6 @@ console.log(
 ); // lux
 ``` */
 
-/* const findBestEmployee = function(employees) {
-
-}; */
-
-/* const employees = {
-  ann: 29,
-  david: 35,
-  helen: 1,
-  lorence: 99,
-};
-console.table(employees);
-
-const entries = Object.entries(employees);
-
-
-for (const entrie of entries) {
-  
-  if(entrie > employees){
-    
-  }
-  console.log(entries);
-} */
 
 const findBestEmployee = function (employees) {
   const numberTasks = Object.values(employees);
