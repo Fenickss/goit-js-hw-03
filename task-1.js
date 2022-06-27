e79c38e775b6e196a7cc110f8e72555c17baa372
Fenickss <denis.suxov.1991@mail.ru>
2022-06-27 11:51:26 +0300

    task1

--- task-1.js
+++ task-1.js
@@ -16,3 +16,24 @@ const user = {
   premium: true,
 };
 ``` */
+
+
+/* 
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,  
+}
+
+user.mood = 'happy';
+//user['mood'] = 'happy'
+user.hobby = 'skydiving';
+user.premium = 'false';
+
+let keys = Object.keys(user);
+
+for (const key of keys) {
+  console.log(key)
+}
+console.log(user); */
