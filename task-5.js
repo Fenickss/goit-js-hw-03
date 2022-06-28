d2360267c8e7410d7224ea2d90ff6e9b586da489
Fenickss <denis.suxov.1991@mail.ru>
2022-06-28 15:25:32 +0300

    task

--- task-5.js
+++ task-5.js
@@ -25,3 +25,30 @@ console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
 
 console.log(getAllPropValues(products, 'category')); // []
 ``` */
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const values = [];
+
+  for (const arrey of arr) {
+    let value = arrey[prop];
+
+    if (value) {
+      values.push(value);
+    }
+  }
+
+  return values;
+};
+
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
