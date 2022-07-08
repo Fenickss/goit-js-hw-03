95f36fd96207484b72f019e27ec39e46e6f98663
Fenickss <denis.suxov.1991@mail.ru>
2022-07-08 21:18:32 +0300

    task-6

--- task-6.js
+++ task-6.js
@@ -26,8 +26,33 @@ console.log(calculateTotalPrice(products, 'Радар')); // 5200
 console.log(calculateTotalPrice(products, 'Дроид')); // 2800
 ``` */
 
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
 const calculateTotalPrice = function(allProdcuts, productName) {
+  let result;
+
+  for (const arr of allProdcuts) {
+    if (arr.name === productName) {
+      result = arr.price * arr.quantity
+    }
+    
+   
+    
+    
+
+    
+  }
  
+return result;
+  
 };
 
-calculateTotalPrice();
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
